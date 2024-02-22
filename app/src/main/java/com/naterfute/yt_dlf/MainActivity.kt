19902ea3b94685d3ac9f85448bd2586e9c2477bc
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-02-21 22:29:26 -0800

    update: Started doing some experimentation with it

--- app/src/main/java/com/naterfute/yt_dlf/MainActivity.kt
+++ app/src/main/java/com/naterfute/yt_dlf/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.naterfute.yt_dlf
 
 import android.os.Bundle
-import android.os.Message
+import android.text.style.BackgroundColorSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.naterfute.yt_dlf.ui.theme.YtdlfTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,8 +31,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = colorScheme.background
                 ) {
+                    GreetingText(message = "I hope you die.", from = "From Me")
                 }
             }
         }
@@ -35,15 +43,34 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun GreetingText(message: String,  modifier: Modifier = Modifier) {
-    Text(
-        text=message
-    )
+fun GreetingText(
+    message: String,
+    from: String,
+    modifier: Modifier = Modifier.padding(8.dp)
+    ) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun GreetingPreview() {
     YtdlfTheme {
+        GreetingText(message = "Oh, It's you.", from = "From Me")
     }
 }
